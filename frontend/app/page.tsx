d6ffa268a839812fbc814da0fe1757454b8149b8
mahmoud <mahmoudyaya33@gmail.com>
2025-09-26 15:37:35 +0200

    Start frontend

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,22 @@
-import Image from "next/image";
+import { useState } from "react";
+import moduleName from "module";
+
+("use client ");
+
+type Transaction = {
+  id: string;
+  text: string;
+  amount: number;
+  created_at: string;
+};
 
 export default function Home() {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const getTransactio = async () => {
+    try {
+      const res = await api.get<Transaction[]>("transactions/");
+      setTransactions(res.data);
+    } catch (error) {}
+  };
   return <button className="btn btn-ms">Test </button>;
 }
